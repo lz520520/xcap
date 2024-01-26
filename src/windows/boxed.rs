ee512d54c6ddda3de2187e757acee7fe3ce9442d
lz520520 <610188221@qq.com>
2024-01-26 10:27:05 +0800

    update

--- src/windows/boxed.rs
+++ src/windows/boxed.rs
@@ -6,10 +6,6 @@ use windows::{
         Graphics::Gdi::{CreateDCW, DeleteDC, DeleteObject, GetWindowDC, ReleaseDC, HBITMAP, HDC},
     },
 };
-use windows::Win32::Foundation::HWND;
-use windows::Win32::Graphics::Gdi::{GetDC, ReleaseDC};
-use windows::Win32::UI::WindowsAndMessaging::GetDesktopWindow;
-use crate::{XCapError, XCapResult};
 
 pub(super) struct BoxHDC {
     hdc: HDC,
@@ -91,38 +87,5 @@ impl Drop for BoxHBITMAP {
 impl BoxHBITMAP {
     pub fn new(h_bitmap: HBITMAP) -> Self {
         BoxHBITMAP(h_bitmap)
-    }
-}
-
-pub(crate) struct BoxMonitorHDC {
-    hwnd: HWND,
-    hdc: HDC,
-}
-impl Deref for BoxMonitorHDC {
-    type Target = HDC;
-    fn deref(&self) -> &Self::Target {
-        &self.hdc
-    }
-}
-
-impl Drop for BoxMonitorHDC {
-    fn drop(&mut self) {
-        unsafe {
-            ReleaseDC(self.hwnd, self.hdc);
-        };
-    }
-}
-
-impl BoxMonitorHDC {
-    pub unsafe  fn new() -> XCapResult<BoxMonitorHDC> {
-        let hwnd = GetDesktopWindow();
-        let hdc = GetDC(hwnd);
-        if hdc.is_invalid() {
-            return Err(XCapError::new("GetDC is failed"))
-        }
-        Ok(BoxMonitorHDC{
-            hwnd,
-            hdc,
-        })
     }
 }
